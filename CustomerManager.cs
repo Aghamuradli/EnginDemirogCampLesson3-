8628cc8a560feeffee4f1b085fc2348b1b2e8adc
Babek Aghamuradov <babek.aghamuradli@gmail.com>
2022-09-17 21:05:54 +0400

    Add files via upload

---
+++ CustomerManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Congratulations. Successfully added : " + customer.Name + " " + customer.Surname + " Balance : " + customer.Balance);
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Congratulations. Successfully deleted : " + customer.Name + " " + customer.Surname);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Name = "Babek";
+            customer1.Surname = "Aghamuradli";
+            customer1.Balance = 234.50;
+
+            Customer customer2 = new Customer();
+            customer2.Name = "Kenan";
+            customer2.Surname = "Idayatov";
+            customer2.Balance = 412.55;
+
+            Customer[] customers = new Customer[] { customer1, customer2};
+
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine(customer.Name);
+                Console.WriteLine(customer.Surname);
+                Console.WriteLine(customer.Balance);
+                Console.WriteLine("---------------");
+            }
+
+
+            CustomerManager customerManager = new CustomerManager();
+
+            Console.WriteLine("---------------Add Method---------------");
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+
+            Console.WriteLine("---------------Delete Method---------------");
+            customerManager.Delete(customer1);
+            customerManager.Delete(customer2);
+        }
+    }
+}
